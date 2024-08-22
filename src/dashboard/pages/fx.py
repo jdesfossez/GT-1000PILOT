e9a88119645f5c3ed11be9aad470942ae9f5846e
Julien Desfossez <j@jdfz.org>
2024-08-22 09:30:30 -0400

    fix text color and refresh component

--- src/dashboard/pages/fx.py
+++ src/dashboard/pages/fx.py
@@ -23,35 +23,46 @@ def serve_layout():
     return html.Div(
         id="button-grid",
         children=[
-            html.Button(
-                id=f"toggle_fx{n}",
+            dcc.Interval(
+                id='interval-component',
+                interval=1*1000, # in milliseconds
+                n_intervals=0
+                ),
+            html.Div(
                 children=[
-                    dcc.Loading(
-                        html.Div(
-                            children=[
-                                html.Img(
-                                    src="/assets/stompbox.png",
-                                    width="80%",
-                                    height="80%",
-                                ),
-                                html.H1(id=f"fx{n}_name", children=gt1000.dash_effects[n-1]["name"]),
-                            ]
+                    html.Button(
+                        id=f"toggle_fx{n}",
+                        children=[
+                            dcc.Loading(
+                                html.Div(
+                                    children=[
+                                        html.Img(
+                                            src="/assets/stompbox.png",
+                                            width="80%",
+                                            height="80%",
+                                            ),
+                                        html.H2(id=f"fx{n}_name", children=gt1000.dash_effects[n-1]["name"]),
+                                        ],
+                                    style={"color": "black"},
+                                    )
+                                )
+                            ],
+                        n_clicks=0,
+                        style={"backgroundColor": gt1000.dash_effects[n-1]["color"]},
                         )
-                    )
-                ],
-                n_clicks=0,
-                style={"backgroundColor": gt1000.dash_effects[n-1]["color"]},
-            )
-            for n in range(1, nr_fx + 1)
-        ],
-        style={
-            "display": "grid",
-            "grid-template-columns": f"repeat({nr_fx}, 1fr)",
-            "width": "100vw",
-            "height": "80vh",
-            "gap": "0",
-        },
-    )
+                    for n in range(1, nr_fx + 1)
+                    ],
+                style={
+                    "display": "grid",
+                    "grid-template-columns": f"repeat({nr_fx}, 1fr)",
+                    "width": "100vw",
+                    "height": "80vh",
+                    "gap": "0",
+                    },
+                ),
+
+            ],
+        )
 
 
 def register_callbacks(app):
